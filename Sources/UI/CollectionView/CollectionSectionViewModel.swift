db22f5816d62d88667edf67d0feb440b53ab5e11
Alexander K <alexander-kad@yandex.ru>
2026-01-31 18:42:54 +0500

    fixes

--- Sources/UI/CollectionView/CollectionSectionViewModel.swift
+++ Sources/UI/CollectionView/CollectionSectionViewModel.swift
@@ -3,14 +3,28 @@ import Combine
 
 open class CollectionSectionViewModel: Hashable, @unchecked Sendable {
     
-    let cellViewModelsSubject = PassthroughSubject<[CollectionCellViewModel], Never>()
+    @Published private(set) var cellViewModels: [CollectionCellViewModel] = []
     
     open var id: String {
         return String(describing: self)
     }
     
+    public init() {
+        
+    }
+    
     public func append(cellViewModels: [CollectionCellViewModel]) {
-        cellViewModelsSubject.send(cellViewModels)
+        var oldCellViewModels = self.cellViewModels
+        let difference = cellViewModels.difference(from: oldCellViewModels)
+        for change in difference {
+            switch change {
+            case .insert(_, let element, _):
+                oldCellViewModels.append(element)
+            case .remove(_, let element, _):
+                oldCellViewModels.removeAll(where: { $0 == element })
+            }
+        }
+        self.cellViewModels = oldCellViewModels
     }
     
     open func hash(into hasher: inout Hasher) {
