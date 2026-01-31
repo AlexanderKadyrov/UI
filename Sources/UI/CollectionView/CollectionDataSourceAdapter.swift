ce802502cc58dfb349a4f83380b47285fb7c4536
Alexander K <alexander-kad@yandex.ru>
2026-01-31 18:42:31 +0500

    fixes

--- Sources/UI/CollectionView/CollectionDataSourceAdapter.swift
+++ Sources/UI/CollectionView/CollectionDataSourceAdapter.swift
@@ -22,13 +22,29 @@ public class CollectionDataSourceAdapter {
     public func append(sections: [CollectionSectionViewModel]) {
         snapshot.appendSections(sections)
         for element in sections {
-            element.cellViewModelsSubject
+            element.$cellViewModels
                 .receive(on: DispatchQueue.main)
                 .sink { [weak self] cellViewModels in
                     guard let self else { return }
-                    snapshot.appendItems(cellViewModels, toSection: element)
+                    refresh(cellViewModels: cellViewModels, section: element)
                 }
                 .store(in: &cancellables)
         }
+    }
+    
+    private func refresh(cellViewModels: [CollectionCellViewModel], section: CollectionSectionViewModel) {
+        let oldCellViewModels = snapshot.itemIdentifiers(inSection: section)
+        let difference = cellViewModels.difference(from: oldCellViewModels)
+        var new = snapshot
+        for change in difference {
+            switch change {
+            case .insert(_, let element, _):
+                guard new.indexOfItem(element) == nil else { continue }
+                new.appendItems([element], toSection: section)
+            case .remove(_, let element, _):
+                new.deleteItems([element])
+            }
+        }
+        snapshot = new
     }
 }
